30fdb325a12a009345ac3e7fc14008c00f22e144
Justin Moore <jmooretenor@gmail.com>
2020-04-11 12:19:12 -0400

    Add assets and character movement script

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // how fast the character can move
+    public float topSpeed = 10f;
+    public bool isGrounded = false;
+
+    // tell the sprite which direction it is pointing
+    bool facingRight = true;
+
+    // physics in fixed update
+    private void FixedUpdate()
+    {
+        Jump();
+        // get move direction
+        float move = Input.GetAxis("Horizontal");
+
+        // add velocity to rigidbody
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * topSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        // if we are facing the negative direciton and, flip
+        if(move > 0 && !facingRight)
+        {
+            FlipSprite();
+        }
+        else if(move < 0 && facingRight)
+        {
+            FlipSprite();
+        }
+    }
+
+    void FlipSprite()
+    {
+        // change sprite direction
+        facingRight = !facingRight;
+
+        // get the local scale
+        Vector3 theScale = transform.localScale;
+
+        // flip on the x axis
+        theScale.x *= -1;
+
+        // apply that to the local scale
+        transform.localScale = theScale;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/Character/Grounded.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject Player;
+
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<CharacterController>().isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<CharacterController>().isGrounded = false;
+        }
+    }
+}
